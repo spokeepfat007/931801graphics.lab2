0e733a65adb2f60b6eb8c1f3aefea8cec515d5c6
spokeepfat007 <Spokeepfat@mail.ru>
2021-10-26 12:51:43 +0700

    Sdal

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.a=[]
         self.width = 961
         self.height = 591
-        self.imagePath = 'anime.jpg'
+        self.imagePath = 'круги.jpg'
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self.radioButton.toggled.connect(self._on_radio_button_clicked)
         self.radioButton_2.toggled.connect(self._on_radio_button_clicked)
@@ -30,11 +30,11 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.pushButton_2.clicked.connect(self.on_click2)
         self.pushButton_3.clicked.connect(self.on_click3)
         self.label.setScaledContents(True)
-        pixmap = QPixmap('anime.jpg')
+        pixmap = QPixmap('круги.jpg')
         pixmap = pixmap.scaled(300, 400, QtCore.Qt.KeepAspectRatio)
         self.pixmap=pixmap
         self.label.setPixmap(pixmap)
-        self.pixmap1 = pixmap
+        self.NumOfCopies = 6;
         self.label.move(100, 100)
 
         self.image = QImage(self.size(), QImage.Format_RGB32)
@@ -45,10 +45,9 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         image = QFileDialog.getOpenFileName(None, 'OpenFile', '', "Image file(*.jpg)")
         self.imagePath = image[0]
         pixmap = QPixmap(self.imagePath)
-        pixmap = pixmap.scaled(300, 400, QtCore.Qt.KeepAspectRatio)
         self.pixmap=pixmap
+        pixmap = pixmap.scaled(300, 400, QtCore.Qt.KeepAspectRatio)
         self.label.setPixmap(pixmap)
-        self.pixmap1 = pixmap
         self.label.adjustSize()
         self.v=0
         self.a=[]
@@ -70,7 +69,6 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         pixmap = pixmap.scaled(300, 400, QtCore.Qt.KeepAspectRatio)
         self.pixmap=pixmap
         self.label.setPixmap(pixmap)
-        self.pixmap1 = pixmap
         self.label.adjustSize()
         self.v=0
         self.a=[]
@@ -124,27 +122,31 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         minY = min(mas[i][1] for i in range(4))
         maxX = max(mas[i][0] for i in range(4))
         maxY = max(mas[i][1] for i in range(4))
-        minX, minY, maxX, maxY = self.findScreenBorders(matrix,[minX,minY,maxX, maxY])
+        minX, minY, maxX, maxY, interX, interY = self.findScreenBorders(matrix,[minX,minY,maxX, maxY])
 
-        return minX,minY, maxX, maxY
+        return minX,minY, maxX, maxY, interX, interY
 
     def switch(self,value, PointsMax, PointsMin, max,min,c,a):
+        if value[0]==0:
+            inter = int(((c/a)-(max-min))/2)
+            return min, max, inter
         if value[0] == 1 and value[1]==1:
             min=PointsMin
             max=PointsMin+ c/a
-            return min, max
+
+            return min, max, 0
         elif value == [1,0,1,1]:
             min = PointsMin-(c/a-(PointsMax-PointsMin))/2
             max = PointsMax+(c/a-(PointsMax-PointsMin))/2
-            return min, max
+            return min, max, 0
         elif value == [1,0,1,0]:
             max = PointsMax+(c/a-(PointsMax-PointsMin)) - (PointsMin-min)
-            return min, max
+            return min, max, 0
         elif value ==[1,0,0,1]:
             min = PointsMin-(c/a-(PointsMax-PointsMin)) - (max-PointsMax)
-            return min, max
+            return min, max, 0
         else:
-            return min, max
+            return min, max, 0
 
     def findScreenBorders(self, matrix, imageBorders):
         minX, minY, maxX, maxY = imageBorders
@@ -155,14 +157,14 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         PointsMaxY = max(NewPoint1[1],NewPoint2[1],NewPoint3[1])
         PointsMinX =min(NewPoint1[0],NewPoint2[0],NewPoint3[0])
         PointsMinY = min(NewPoint1[1],NewPoint2[1],NewPoint3[1])
-        value = [((maxX-minX)>self.width/2),(PointsMaxX-PointsMinX)>self.width/2,(self.width/2-(PointsMaxX-PointsMinX))/2<(maxX-PointsMaxX),(self.width/2-(PointsMaxX-PointsMinX))/2<(PointsMinX-minX)]
-        minX, maxX = self.switch(value,PointsMaxX, PointsMinX, maxX,minX,self.width,2)
-        value = [((maxY-minY)>self.height/2),(PointsMaxY-PointsMinY)>self.height/2,(self.height/2-(PointsMaxY-PointsMinY))/2<(maxY-PointsMaxY),(self.height/2-(PointsMaxY-PointsMinY))/2<(PointsMinY-minY)]
-        minY, maxY = self.switch(value,PointsMaxY, PointsMinY, maxY,minY,self.height,1)
-        return minX, minY, maxX, maxY
+        value = [((maxX-minX)>(self.width/2)),(PointsMaxX-PointsMinX)>self.width/2,(self.width/2-(PointsMaxX-PointsMinX))/2<(maxX-PointsMaxX),(self.width/2-(PointsMaxX-PointsMinX))/2<(PointsMinX-minX)]
+        minX, maxX, interX = self.switch(value,PointsMaxX, PointsMinX, maxX,minX,self.width,2)
+        value = [(maxY-minY)>self.height,(PointsMaxY-PointsMinY)>self.height,(self.height-(PointsMaxY-PointsMinY))<(maxY-PointsMaxY),(self.height-(PointsMaxY-PointsMinY))<(PointsMinY-minY)]
+        minY, maxY, interY= self.switch(value,PointsMaxY, PointsMinY, maxY,minY,self.height,1)
+        return minX, minY, maxX, maxY, interX, interY
 
     def simpleTransformation(self, imageBorders, ObrMatrix, painter, image_data):
-        minX, minY, maxX, maxY = imageBorders
+        minX, minY, maxX, maxY, interX, interY = imageBorders
         for i in range(int(minX),int(maxX)):
             for j in range(int(minY), int(maxY)):
                 z = np.array([i, j, 1]).dot(ObrMatrix)
@@ -171,11 +173,11 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 else:
                     r, g, b = image_data[int(z[1])*self.label.width()+int(z[0])]
                     painter.setPen(QPen(QColor(r,g,b), 1, Qt.SolidLine))
-                painter.drawPoint(i+481-minX, j+1-minY)
+                painter.drawPoint(i+481-int(minX)+interX, j+1-int(minY)+interY)
                 self.update()
 
     def bilinearFiltr(self, imageBorders, ObrMatrix, painter, image_data):
-        minX, minY, maxX, maxY = imageBorders
+        minX, minY, maxX, maxY, interX, interY = imageBorders
         for i in range(int(minX),int(maxX),1):
             for j in range(int(minY), int(maxY),1):
                 z = np.array([i, j, 1]).dot(ObrMatrix)
@@ -186,17 +188,33 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                              np.float_(image_data[math.ceil(z[1])*self.label.width()+math.floor(z[0])])*(math.ceil(z[0])-z[0]))*(z[1]-math.floor(z[1])) + \
                             (np.float_(image_data[math.floor(z[1])*self.label.width()+math.ceil(z[0])])*(z[0]-math.floor(z[0])) +
                              np.float_(image_data[math.floor(z[1])*self.label.width()+math.floor(z[0])])*(math.ceil(z[0])-z[0]))*(math.ceil(z[1])-z[1])
-                    # r, g, b = [round(i) for i in(colours)] #Целочисленное округление, если нужно
+                   #r, g, b = [round(i) for i in([r,g,b])] #Целочисленное округление, если нужно
                     painter.setPen(QPen(QColor(r,g,b), 1, Qt.SolidLine))
-                painter.drawPoint(i+481-minX, j+1-minY)
+                painter.drawPoint(i+481-int(minX)+interX, j+1-int(minY)+interY)
                 self.update()
 
     def trilinearFiltr(self, imageBorders, ObrMatrix, painter, image_data, image):
-        minX, minY, maxX, maxY = imageBorders
+        minX, minY, maxX, maxY, interX, interY  = imageBorders
         matrixes=[image_data]
-        for i in range(1, 4):
-            power = pow(2,i)
-            matrixes+=[image.resize((math.ceil(self.label.size().width()/power), math.ceil(self.label.size().height()/power))).getdata()]
+        # for i in range(1, self.NumOfCopies):
+        #     power = pow(2,i)
+        #     matrixes+=[image.resize((math.ceil(self.label.size().width()/power), math.ceil(self.label.size().height()/power))).getdata()]
+        for power in range(1, self.NumOfCopies):
+            power = pow(2,power)
+            new_picture=[]
+            for j in range(0, self.label.height(), power):
+                for i in range(0, self.label.width(), power):
+                    s= np.zeros(shape=3)
+                    maxi=power if ((self.label.width()-power)>=i)else (self.label.width()-i)
+                    maxj=power if ((self.label.height()-power)>=j) else (self.label.height()-j)
+                    for i1 in range(0,maxi):
+                        for i2 in range(0,maxj):
+                            s+=np.array(image_data[(j+i2)*self.label.width()+(i1+i)])
+                    for i1 in range(0,3):
+                        s[i1]/=maxi*maxj
+                    new_picture+=[s]
+            matrixes+=[new_picture]
+
         for i in range(int(minX),int(maxX)):
             for j in range(int(minY), int(maxY)):
                 z = np.array([i, j, 1]).dot(ObrMatrix)
@@ -206,32 +224,30 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     distance = np.array([i+1, j, 1]).dot(ObrMatrix)-np.array([i, j, 1]).dot(ObrMatrix)
                     K = (abs(distance[0])+abs(distance[1]))/2
                     m=0
-                    if (K>=8):
-                        K=8
+                    if (K>=math.pow(2,self.NumOfCopies-1)):
+                        K=math.pow(2,self.NumOfCopies-1)
                     elif(K<=1):
                         K=1
                     if K<=2:
                         m=0
-                    elif K<=4:
-                        m=1
-                    elif K<=8:
-                        m=2
+                    else:
+                        m = math.ceil(math.log2(K))-1
 
-                    Im = (np.float_(matrixes[m][int(math.ceil(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+math.ceil(z[0])/pow(2,m))])*(z[0]-math.floor(z[0])) +
-                             np.float_(matrixes[m][int(math.ceil(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+math.floor(z[0])/pow(2,m))])*(math.ceil(z[0])-z[0]))*(z[1]-math.floor(z[1])) + \
-                            (np.float_(matrixes[m][int(math.floor(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+math.ceil(z[0])/pow(2,m))])*(z[0]-math.floor(z[0])) +
-                             np.float_(matrixes[m][int(math.floor(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+math.floor(z[0])/pow(2,m))])*(math.ceil(z[0])-z[0]))*(math.ceil(z[1])-z[1])
-                    #Im=matrixes[m][int(z[1]/pow(2,m))*math.ceil(self.label.width()/pow(2,m))+int(z[0]/pow(2,m))]
-                    I2m = (np.float_(matrixes[m+1][int(math.ceil(z[1])//(2*pow(2,m))*math.ceil(self.label.width()/(2*pow(2,m)))+math.ceil(z[0])/(2*pow(2,m)))])*(z[0]-math.floor(z[0])) +
-                          np.float_(matrixes[m+1][int(math.ceil(z[1])//(2*pow(2,m))*math.ceil(self.label.width()/(2*pow(2,m)))+math.floor(z[0])/(2*pow(2,m)))])*(math.ceil(z[0])-z[0]))*(z[1]-math.floor(z[1])) + \
-                         (np.float_(matrixes[m+1][int(math.floor(z[1])//(2*pow(2,m))*math.ceil(self.label.width()/(2*pow(2,m)))+math.ceil(z[0])/(2*pow(2,m)))])*(z[0]-math.floor(z[0])) +
-                          np.float_(matrixes[m+1][int(math.floor(z[1])//(2*pow(2,m))*math.ceil(self.label.width()/(2*pow(2,m)))+math.floor(z[0])/(2*pow(2,m)))])*(math.ceil(z[0])-z[0]))*(math.ceil(z[1])-z[1])
-                    #I2m=matrixes[2*m][int(int(z[1]/(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int(z[0])/(2*pow(2,m)))]
+                    Im = (np.float_(matrixes[m][math.ceil(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+int(math.ceil(z[0])/pow(2,m))])*(z[0]-math.floor(z[0])) +
+                             np.float_(matrixes[m][math.ceil(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+int(math.floor(z[0])/pow(2,m))])*(math.ceil(z[0])-z[0]))*(z[1]-math.floor(z[1])) + \
+                            (np.float_(matrixes[m][math.floor(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+int(math.ceil(z[0])/pow(2,m))])*(z[0]-math.floor(z[0])) +
+                             np.float_(matrixes[m][math.floor(z[1])//pow(2,m)*math.ceil(self.label.width()/pow(2,m))+int(math.floor(z[0])/pow(2,m))])*(math.ceil(z[0])-z[0]))*(math.ceil(z[1])-z[1])
+                    Im=matrixes[m][int(z[1]/pow(2,m))*math.ceil(self.label.width()/pow(2,m))+int(z[0]/pow(2,m))]
+                    I2m = (np.float_(matrixes[m+1][int(math.ceil(z[1])//(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int(math.ceil(z[0])/(2*pow(2,m)))])*(z[0]-math.floor(z[0])) +
+                          np.float_(matrixes[m+1][int(math.ceil(z[1])//(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int(math.floor(z[0])/(2*pow(2,m)))])*(math.ceil(z[0])-z[0]))*(z[1]-math.floor(z[1])) + \
+                         (np.float_(matrixes[m+1][int(math.floor(z[1])//(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int(math.ceil(z[0])/(2*pow(2,m)))])*(z[0]-math.floor(z[0])) +
+                          np.float_(matrixes[m+1][int(math.floor(z[1])//(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int(math.floor(z[0])/(2*pow(2,m)))])*(math.ceil(z[0])-z[0]))*(math.ceil(z[1])-z[1])
+                    #I2m=matrixes[m+1][int(z[1]/(2*pow(2,m)))*math.ceil(self.label.width()/(2*pow(2,m)))+int((z[0])/(2*pow(2,m)))]
                     color=[]
                     for rad in range(0,3):
-                        color+=[(Im[rad]*((2*pow(2,m)-K)/pow(2,m))+I2m[rad]*(K-pow(2,m)))/pow(2,m)]
+                        color+=[(Im[rad]*(2*pow(2,m)-K)+I2m[rad]*(K-pow(2,m)))/pow(2,m)]
                     painter.setPen(QPen(QColor(color[0],color[1],color[2]), 1, Qt.SolidLine))
-                painter.drawPoint(i+481-minX, j+1-minY)
+                painter.drawPoint(i+481-int(minX)+interX, j+1-int(minY)+interY)
                 self.update()
 
     def IncreaseDecrease(self,matrix,obrmatr):
@@ -248,7 +264,7 @@ class MainApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         image = image.resize((self.label.size().width(), self.label.size().height()))
         #image.show()
         image_data = image.getdata()
-        #self.a=[58, 346, 168, 71, 210, 355, 42, 142, 68, 90, 104, 145]
+        #self.a=[34, 316, 122, 83, 183, 324, 113, 240, 138, 198, 139, 238]
         a5 = np.array([[self.a[i-i%2+j if i%2==0 else i-i%2+j-3] if j<2 and i%2==0 or j>2 and j<5 and i%2==1 else 1 if j==2 and i%2==0 or j==5 and i%2==1 else 0 for j in range(6)] for i in range(6)])
         v5 = np.array([self.a[6+i] for i in range(6)])
         # print(v5, a5, sep='\n')
